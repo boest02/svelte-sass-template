8eca54ee1ee2c16db7e4636e1c0172dfcc992e84
Boettcher, Stephen <sboettcher@pch.com>
2021-01-23 23:06:03 -0500

    fixes and styles

--- src/App.svelte
+++ src/App.svelte
@@ -2,24 +2,34 @@
   export let name;
 </script>
 
-<style type="text/scss">
-  $color: red;
-
-  h1 {
-    color: $color;
-  }
-
-  div {
-    background: green;
-
-    > p {
-      color: #fff;
-    }
-  }
-</style>
 
 <h1>Hello {name}!</h1>
 
 <div>
   <p>SASS is working!</p>
 </div>
+
+
+<style type="text/scss">
+  $color: red;
+
+  h1 {
+    color: $color;
+    padding: 20px;
+  }
+
+  div {
+    background: green;
+    width: 90%;
+    padding: 15px;
+    margin: 10px auto;
+
+    > p {
+      color: #fff;
+      font-weight: 800;
+      font-size: 1em;
+      text-align: center;
+    }
+  }
+</style>
+
